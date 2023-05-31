c547e2bcf75fe76c282af6159301f29ffb584d88
Mdeclaude <declaudemarcus@gmail.com>
2023-05-31 16:05:12 +0200

    test

--- connexion.php
+++ connexion.php
@@ -1,9 +1,10 @@
+
 <?php
 // Informations de connexion à la base de données
 $serveur = "https://node90-eu.n0c.com/phpmyadmin/index.php?route=/sql&pos=0&db=kxbshafa_CarGoFest&table=Connexion"; // Adresse du serveur MySQL
 $utilisateur = "kxbshafa_Joseph"; // Nom d'utilisateur MySQL
 $motDePasse = "BYC8qeaGyswSZ9"; // Mot de passe MySQL
-$baseDeDonnees = "connexion"; // Nom de la base de données
+$baseDeDonnees = "Connexion"; // Nom de la base de données
 
 // Connexion à la base de données
 $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
@@ -22,7 +23,7 @@ $login = $connexion->real_escape_string($login);
 $password = $connexion->real_escape_string($password);
 
 // Requête d'insertion des données dans la table
-$sql = "INSERT INTO utilisateurs (login, password) VALUES ('$login', '$password')";
+$sql = "INSERT INTO Connexion (login, password) VALUES ('$login', '$password')";
 
 if ($connexion->query($sql) === TRUE) {
     echo "Enregistrement réussi.";
@@ -34,19 +35,4 @@ if ($connexion->query($sql) === TRUE) {
 $connexion->close();
 ?>
 
-<html>
-<head>
-    <title>Connexion</title>
-</head>
-<body>
-    <h2>Veuillez rentre votre nom d'utilisateur et votre mot de passe</h2>
-    <form method="POST" action="connexion.php">
-        
-        <label for="login">Login :</label>
-        <input type="text" name="login" id="login" required></br>
-        <label for="password">Password :</label>
-        <input type="password" name="password" id="password" required></br>
-        <input type="submit" value="Envoyer">
-    </form>
-</body>
-</html>
+
