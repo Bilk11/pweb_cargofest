245c802d880dde1d44dabdfb21d17b410f28f7ff
jtartivel <joseph.tartivel@etudiant.univ-rennes1.fr>
2023-06-05 16:57:33 +0200

    ajout recherche

---
+++ affiche_recherche.php
@@ -0,0 +1,67 @@
+<?php
+$servername = "127.0.0.1";
+$database = "kxbshafa_CarGoFest";
+$username = "kxbshafa_marcus";
+$password = "Basededonnee1234";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT places, date_aller, date_retour FROM Recherche";
+    $stmt = $conn->query($sql);
+
+    if ($stmt->rowCount() > 0) {
+        echo "<table>";
+        echo "<tr><th>Places</th><th>Date Aller</th><th>Date Retour</th></tr>";
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            echo "<td>" . $row["places"] . "</td>";
+            echo "<td>" . $row["date_aller"] . "</td>";
+            echo "<td>" . $row["date_retour"] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Aucune annonce trouvée.";
+    }
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+$conn = null;
+?>
+<html>
+
+<head>
+    <title>Tableau des annonces</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            font-size: 16px;
+        }
+
+        table th,
+        table td {
+            padding: 12px;
+            text-align: left;
+            border: 1px solid #ccc;
+        }
+
+        table th {
+            background-color: #f2f2f2;
+        }
+
+        table tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        table tr:hover {
+            background-color: #eaeaea;
+        }
+    </style>
+</head>
+<html>
---
+++ ajouter_recherche.php
@@ -0,0 +1,69 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "127.0.0.1";
+$database = "kxbshafa_CarGoFest";
+$username = "kxbshafa_marcus";
+$password = "Basededonnee1234";
+$nombreAleatoire = mt_rand(1, 10000);
+
+
+try {
+    session_start();
+    if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
+        header('Location: identification.html');
+        exit();
+    }
+
+    // Création d'une connexion PDO
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+    // Configuration des options de PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Préparation de la requête d'insertion
+    $stmt = $conn->prepare("INSERT INTO  (id,id_festival,id_connexion,places,recherche_aller,recherche_retour) VALUES (:id,:id_festival, :id_connexion, :places, :recherche_aller, :recherche_retour)");
+
+    // Paramètres de la requête
+    $place = $_POST['place_dispo'];
+    $recherche_depart = $_POST['date_depart'];
+    $recherche_retour = $_POST['date_retour'];
+    $id_connexion = "SELECT password FROM Connexion WHERE login = :login";
+
+
+
+    // Attribution des valeurs des paramètres
+    $stmt->bindParam(':id', $nombreAleatoire);
+    $stmt->bindParam(':id_festival', $nombreAleatoire);
+    $stmt->bindParam(':places', $place);
+
+    $stmt->bindParam(':date_aller', $date_depart);
+    $stmt->bindParam(':date_retour', $date_retour);
+
+
+    // Exécution de la requête
+    $stmt->execute();
+
+
+    if (!empty($place)) {
+        // Effectuer l'enregistrement dans la base de données
+        // Votre code d'enregistrement ici
+        // Vérifier si l'enregistrement a réussi
+        // Redirection vers une autre page en cas de succès
+        header("Location: recherche.html");
+        exit;
+
+    } else {
+        header("Location: ajout_recherche.html?erreur=1");
+        exit;
+    }
+
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+// Fermeture de la connexion
+$conn = null;
+?>
