815344d8af12c29f34f778abc7f04c49da7e9280
jtartivel <joseph.tartivel@etudiant.univ-rennes1.fr>
2023-06-05 17:19:27 +0200

    uv

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php
 session_start();
-            echo 'Bonjour ' .$_SESSION['username'];
-            if ( $_SESSION['admin'] === true) {
-                // L'utilisateur est connecté en tant qu'administrateur
-                echo "<a href='table_utilisateur.php'>table utilisateur</a>";
+echo 'Bonjour ' . $_SESSION['username'];
+if ($_SESSION['admin'] === true) {
+    // L'utilisateur est connecté en tant qu'administrateur
+    echo "<a href='table_utilisateur.php'>table utilisateur</a>";
 
-                // Le contenu de la page d'administration peut être affiché ici
-            }
-            if ($_SESSION['loggedIn'] !== true) {
-                echo "<a href='identification.html'>S'identifier</a>";
+    // Le contenu de la page d'administration peut être affiché ici
+}
+if ($_SESSION['loggedIn'] !== true) {
+    echo "<a href='identification.html'>S'identifier</a>";
 
-                }else{
-                    echo "";
-                }
-        ?>
+} else {
+    echo "";
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
-    
+
 </head>
+
 <body>
     <!--<a href="connexion.html" class="button">Se connecter</a></br>
     <a href="festival.html" class="button">Ajouter un festival</a></br>
@@ -28,12 +30,13 @@ session_start();
     <!--<button type="button" onclick="window.location.href = 'identification.html';">S'identifier</button>-->
     <button type="button" onclick="window.location.href = 'festival.html';">Ajouter un festival</button>
     <button type="button" onclick="window.location.href = 'annonce_vehicule.html';">Ajouter un trajet</button>
-
-    <form action="deconnexion.php" >
+    <button type="button" onclick="window.location.href = 'recherche.html';">Ajouter une recherche de trajet</button>
+    <form action="deconnexion.php">
         <button type="submit">Se déconnecter</button>
     </form>
-    
+
 
 
 </body>
+
 </html>
