8b051b76b9f2d6ee3f2ad1087354d96a3d685c22
Mdeclaude <declaudemarcus@gmail.com>
2023-06-06 18:19:46 +0200

    fin 06/06/2023

--- edit.php
+++ edit.php
@@ -2,68 +2,77 @@
 $servername = "127.0.0.1";
 $database = "kxbshafa_CarGoFest";
 $username = "kxbshafa_marcus";
-$password = "Basededonnee1234";
-$pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$passwordbase = "Basededonnee1234";
+$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $passwordbase);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$id = $_GET['id'];
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom_festival'], $_POST['localisation'], $_POST['debut_festival'], $_POST['fin_festival'])) {
-    // Perform the update operation
-    $nom_festival = $_POST['nom_festival'];
-$localisation = $_POST['localisation'];
-$debut_festival = $_POST['debut_festival'];
-$fin_festival = $_POST['fin_festival'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST["id"];
+    $nomFestival = $_POST["nom_festival"];
+    $localisation = $_POST["localisation"];
+    $debutFestival = $_POST["debut_festival"];
+    $finFestival = $_POST["fin_festival"];
 
-$updateQuery = "UPDATE Festival SET nom_festival = :nom, localisation = :localisation, debut_festival = :debut, fin_festival = :fin WHERE id = :id";
-$updateStmt = $pdo->prepare($updateQuery);
-$updateStmt->bindParam(':nom', $nom_festival);
-$updateStmt->bindParam(':localisation', $localisation);
-$updateStmt->bindParam(':debut', $debut_festival);
-$updateStmt->bindParam(':fin', $fin_festival);
-$updateStmt->bindParam(':id', $id);
-$updateStmt->execute();
+    $updateQuery = "UPDATE Festival SET nom_festival = :nom_festival, localisation = :localisation, debut_festival = :debut_festival, fin_festival = :fin_festival WHERE id = :id";
+    $updateStmt = $conn->prepare($updateQuery);
+    $updateStmt->bindParam(':nom_festival', $nomFestival);
+    $updateStmt->bindParam(':localisation', $localisation);
+    $updateStmt->bindParam(':debut_festival', $debutFestival);
+    $updateStmt->bindParam(':fin_festival', $finFestival);
+    $updateStmt->bindParam(':id', $id);
+    $updateStmt->execute();
 
-$selectQuery = "SELECT nom_festival, localisation, debut_festival, fin_festival FROM Festival WHERE id = :id";
-$selectStmt = $pdo->prepare($selectQuery);
-$selectStmt->bindParam(':id', $id);
-$selectStmt->execute();
-
-$row = $selectStmt->fetch(PDO::FETCH_ASSOC);
-
-$nom_festival = $row['nom_festival'];
-$localisation = $row['localisation'];
-$debut_festival = $row['debut_festival'];
-$fin_festival = $row['fin_festival'];
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom_festival'], $_POST['localisation'], $_POST['debut_festival'], $_POST['fin_festival'])) {
-
-
-    //  redirige vers la page aui vient d'etre modif
-    header("Location: festival.html");
+    header("Location: festival.html"); // Redirige vers la page table_festival.php après la modification
     exit();
+} else {
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+        $selectQuery = "SELECT id, nom_festival, localisation, debut_festival, fin_festival FROM Festival WHERE id = :id";
+        $selectStmt = $conn->prepare($selectQuery);
+        $selectStmt->bindParam(':id', $id);
+        $selectStmt->execute();
+        $row = $selectStmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            $id = $row["id"];
+            $nomFestival = $row["nom_festival"];
+            $localisation = $row["localisation"];
+            $debutFestival = $row["debut_festival"];
+            $finFestival = $row["fin_festival"];
+        } else {
+            echo "Festival non trouvé.";
+            exit();
+        }
+    } else {
+        echo "Identifiant du festival non spécifié.";
+        exit();
+    }
 }
-}
-
-
-
 ?>
+
+<!DOCTYPE html>
 <html>
-<form action="edit.php?id=<?php echo $id; ?>" method="POST">
-    <label for="nom_festival">Nom du festival:</label>
-    <input type="text" name="nom_festival" value="<?php echo $nom_festival; ?>">
-    <br>
-    <label for="localisation">Localisation:</label>
-    <input type="text" name="localisation" value="<?php echo $localisation; ?>">
-    <br>
-    <label for="debut_festival">Début du festival:</label>
-    <input type="date" name="debut_festival" value="<?php echo $debut_festival; ?>">
-    <br>
-    <label for="fin_festival">Fin du festival:</label>
-    <input type="date" name="fin_festival" value="<?php echo $fin_festival; ?>">
-    <br>
-    <input type="submit" value="Modifier">
+<head>
+    <title>Modifier un festival</title>
+</head>
+<body>
+    <h2>Modifier un festival</h2>
 
-</form>
-
-    </html>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <label for="nom_festival">Nom du festival:</label>
+        <input type="text" name="nom_festival" value="<?php echo $nomFestival; ?>">
+        <br>
+        <label for="localisation">Localisation:</label>
+        <input type="text" name="localisation" value="<?php echo $localisation; ?>">
+        <br>
+        <label for="debut_festival">Début du festival:</label>
+        <input type="date" name="debut_festival" value="<?php echo $debutFestival; ?>">
+        <br>
+        <label for="fin_festival">Fin du festival:</label>
+        <input type="date" name="fin_festival" value="<?php echo $finFestival; ?>">
+        <br>
+        <input type="submit" value="Modifier">
+    </form>
+</body>
+</html>
